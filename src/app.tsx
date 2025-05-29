94672fa704211738785ad3a552c599a48a0c325f
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2025-05-28 23:58:31 -0300

    Primeiro commit

---
+++ src/app.tsx
@@ -0,0 +1,58 @@
+import { CreateMLCEngine, MLCEngine } from "@mlc-ai/web-llm";
+import { useEffect, useState } from "react";
+
+export function App() {
+  const [engine, setEngine] = useState<MLCEngine>();
+  const [progress, setProgress] = useState(0);
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [generating, setGenerating] = useState(false);
+
+  useEffect(() => {
+    CreateMLCEngine("Llama-3.2-1B-Instruct-q4f16_1-MLC", {
+      initProgressCallback: ({ progress }) => setProgress(progress),
+    }).then(setEngine);
+  }, []);
+
+  async function chat() {
+    if (!engine) return;
+
+    setGenerating(true);
+    setOutput("");
+
+    const chunks = await engine.chat.completions.create({
+      messages: [
+        { role: "system", content: "Responda de forma clara e objetiva sempre em português brasileiro" },
+        { role: "user", content: input },
+      ],
+      temperature: 1,
+      stream: true,
+      stream_options: { include_usage: true },
+    });
+
+    for await (const chunk of chunks) {
+      setOutput((prev) => prev + (chunk.choices[0]?.delta.content || ""));
+      if (chunk.usage) console.log(chunk.usage);
+    }
+
+    setGenerating(false);
+    setInput("");
+  }
+
+  if (!engine) return <p>Carregando {progress}%</p>;
+
+  return (
+    <main>
+      <form action={chat} autoComplete="off">
+        <label>
+          Input:
+          <input type="text" name="input" value={input} onChange={(e) => setInput(e.target.value)} disabled={generating} />
+        </label>
+        <button type="submit" disabled={generating}>
+          Enviar
+        </button>
+      </form>
+      <pre dangerouslySetInnerHTML={{ __html: output }}></pre>
+    </main>
+  );
+}
